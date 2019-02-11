282dbe737a0574463914c724d6acd9442bba63f5
Peter Zhang <43096010+ztylove61@users.noreply.github.com>
2019-02-11 20:21:37 +0000

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con = mysqli_connect('localhost','root','');
+$con = mysqli_connect('localhost','root','password');
 mysqli_select_db($con,'epic');
 $name = $_POST['user'];
 $pass = $_POST['password'];
